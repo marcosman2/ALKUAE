1ff99218122e5f80d4f6fe015d9a4ef88a3e9199
M-MANRIQUE\Marcos Manrique <marcos.manrique@stellarelements.com>
2023-08-08 09:18:08 -0500

    Dimension class

--- src/main/java/Pages/LastPassPage.java
+++ src/main/java/Pages/LastPassPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.Dimension;
 
 public class LastPassPage extends Wrappers {
 
